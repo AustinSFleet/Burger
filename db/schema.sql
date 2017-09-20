571a7f08d89ec5567524a0d6eaa30ec732dd21b2
AustinSFleet <austinsfleet@gmail.com>
2017-09-19 19:45:02 -0500

    added sql schema and seed file scripts

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR (200) NULL,
+    devoured BOOLEAN,
+    date TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers(burger_name, devoured)
+    VALUES ("double burger", false),
+            ("big kahuna burger", false),
+            ("good-burger burger", false);
